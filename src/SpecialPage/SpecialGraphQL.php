5f06fd00e050f364ab09796a43b69648f694bdb8
David Barratt <dbarratt@wikimedia.org>
2019-04-13 13:13:57 -0400

    Convert SpecialPage to a service
    The SpecialPage has dependencies. Rather than making it aware of the container
    the SpecialPage should become a service.
    
    Change-Id: I265bde52a44e92c2287e3cd72dc3a2a585b4fed2

--- src/SpecialPage/SpecialGraphQL.php
+++ src/SpecialPage/SpecialGraphQL.php
@@ -3,11 +3,40 @@
 namespace MediaWiki\GraphQL\SpecialPage;
 
 use GraphQL\GraphQL;
-use MediaWiki\MediaWikiServices;
+use GraphQL\Executor\Promise\PromiseAdapter;
+use GraphQL\Type\Schema;
+use MediaWiki\Linker\LinkRenderer;
 
 class SpecialGraphQL extends \UnlistedSpecialPage {
-	public function __construct() {
+
+	/**
+	 * @var PromiseAdapter
+	 */
+	protected $promise;
+
+	/**
+	 * @var Schema;
+	 */
+	protected $schema;
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @param LinkRenderer $linkRenderer
+	 * @param PromiseAdapter $promise
+	 * @param Schema $schema
+	 */
+	public function __construct(
+		LinkRenderer $linkRenderer,
+		PromiseAdapter $promise,
+		Schema $schema
+	) {
 		parent::__construct( 'GraphQL' );
+
+		$this->promise = $promise;
+		$this->schema = $schema;
+
+		$this->setLinkRenderer( $linkRenderer );
 	}
 
 	/**
@@ -16,8 +45,6 @@ class SpecialGraphQL extends \UnlistedSpecialPage {
 	 * @param string|null $subPage
 	 */
 	public function execute( $subPage ) {
-		$schema = MediaWikiServices::getInstance()->getService( 'GraphQLSchema' );
-
 		if ( $this->getRequest()->wasPosted() ) {
 			$raw = $this->getRequest()->getRawPostString();
 			$input = \FormatJson::parse( $raw, \FormatJson::FORCE_ASSOC )->getValue();
@@ -32,16 +59,15 @@ class SpecialGraphQL extends \UnlistedSpecialPage {
 		}
 
 		try {
-				$adapter = MediaWikiServices::getInstance()->getService( 'GraphQLPromiseAdapter' );
 				$promise = GraphQL::promiseToExecute(
-					$adapter,
-					$schema,
+					$this->promise,
+					$this->schema,
 					$query,
 					null,
 					null,
 					$variables
 				);
-				$result = $adapter->wait( $promise );
+				$result = $this->promise->wait( $promise );
 				$output = $result->toArray( true );
 		} catch ( \Exception $e ) {
 				$output = [
@@ -53,15 +79,36 @@ class SpecialGraphQL extends \UnlistedSpecialPage {
 				];
 		}
 
-		// Disable the Page output.
-		// @TODO If the user's `Accept` header is `text/html` respond with an
-		// in-browser GraphQL IDE.
-		$this->getOutput()->disable();
+		$header = $this->getRequest()->getHeader( 'Accept', \WebRequest::GETHEADER_LIST );
+		$accept = [];
+		if ( is_array( $header ) ) {
+			$accept = $header;
+		} elseif ( is_string( $header ) ) {
+			$accept = [ $header ];
+		}
 
-		$response = $this->getRequest()->response();
-		$response->header( 'Access-Control-Allow-Origin: *' );
-		$response->header( 'Content-Type: application/json' );
+		$accept = array_map( function ( $item ) {
+			$split = explode( ';', $item );
+			return $split[0];
+		}, $accept );
 
-		print \FormatJson::encode( $output, false, \FormatJson::ALL_OK );
+		// The default response is JSON.
+		if (
+			empty( $accept ) ||
+			in_array( 'application/json', $accept, true ) ||
+			$accept === [ '*/*' ]
+		) {
+			// Disable the Page output.
+			$this->getOutput()->disable();
+			$response = $this->getRequest()->response();
+			$response->header( 'Access-Control-Allow-Origin: *' );
+			$response->header( 'Content-Type: application/json' );
+
+			print \FormatJson::encode( $output, false, \FormatJson::ALL_OK );
+		}
+
+		// @TODO If the user's `Accept` header is anything else, response with an
+		// in-browser IDE.
+		$this->getOutput()->setPageTitle( $this->msg( 'graphql' ) );
 	}
 }
